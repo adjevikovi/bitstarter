4087d619810d645876513738ce0952760a473393
pedrospaulus <pedrospaulus@gmail.com>
2013-07-25 16:26:55 +0000

    grader.js and checks.json added ...

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var  rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var DEFAULT_URL = "http://www.google.com"
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,8 +38,32 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
+var assertUrlExists = function(inurl) {
+    var instr = inurl.toString();
+    
+    rest.get(instr).on('complete', function(data) {
+	if(data instanceof Error){
+	    console.log("This url seem to not exist. Please check it");
+	    process.exit(1);
+	}
+    });
+    return instr;
+};
+
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
+};
+
+var restlerUrlFile = function(innerUrl) {
+    var outputData;
+    
+    rest.get(innerUrl).on('complete', function(data){
+      if(!(data instanceof Error)){
+        console.log("Fetched data from " + innerUrl+ " is : " + data)
+        outputData = cheerio.load(data);
+      }
+    })
+    return outputData;
 };
 
 var loadChecks = function(checksfile) {
@@ -55,6 +81,18 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkUrlFile = function(urlfile, checksfile) {
+    $ = restlerUrlFile(urlfile);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,10 +103,24 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+     .option('-u, --url <url_file>', 'Path to url content', clone(assertUrlExists), DEFAULT_URL)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if(program.file){
+          var checkJson = checkHtmlFile(program.file, program.checks);
+          var outJson = JSON.stringify(checkJson, null, 4);
+          console.log(outJson);
+    } else if(program.url){
+      console.log("this one is the url parameter case !! ");
+          var checkJson = checkUrlFile(program.url, program.checks);
+          var outJson = JSON.stringify(checkJson, null, 4);
+          console.log(outJson);
+    } 
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+    if(program.file){
+          exports.checkHtmlFile = checkHtmlFile;
+    } else if(program.url){
+      console.log("this one is the url parameter case !! ");
+	exports.checkUrlFile = checkUrlFile;
+    }
 }
